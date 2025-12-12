10662a3c664d8c5701e06130b6c8a89f91e618ed
haolamnm <haolamnm@gmail.com>
2025-12-13 01:47:03 +0700

    fix: remove horizontal white line at bottom padding of body tag

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -48,7 +48,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
 
             <Navigation />
 
-            <main className="relative z-10 pt-20 pb-32">
+            <main className="relative z-10 pt-20 pb-32 min-h-screen">
                 <div className="container mx-auto px-4 md:px-6 lg:px-8 max-w-6xl">
                     {children}
                 </div>
